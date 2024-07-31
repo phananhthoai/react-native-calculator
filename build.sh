023fd76a189dc9e1242808bf69796488004bb61a
phananhthoai <kenkukhanh@gmail.com>
2024-07-31 10:34:43 +0000

    edit

--- build.sh
+++ build.sh
@@ -7,5 +7,12 @@ echo "Build"
 if [ -f yarn.lock ]; then
   yarn
 fi
-eas build --local --platform=android --output=calculator-$(date +'%Y-%M-%d-%H-%M-%s').apk
-  
+
+output_path="calculator-$(date +'%Y-%M-%d-%H-%M-%s').apk"
+eas build --local --platform=android --output=${output_path} 
+
+if [ ! -f ${output_path} ]; then
+  exit 1
+fi
+
+echo "${output_path}" | tee output.txt
